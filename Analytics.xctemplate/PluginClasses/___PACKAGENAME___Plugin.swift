5cee7f6df1785321d8639ea467c1a63d0ee71214
Roi <roiked@gmail.com>
2018-08-13 14:18:33 +0300

    initial commit

---
+++ Analytics.xctemplate/PluginClasses/___PACKAGENAME___Plugin.swift
@@ -0,0 +1,16 @@
+//
+//  ___PACKAGENAME___Plugin
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import Foundation
+import ZappPlugins
+import ApplicasterSDK
+
+public class ___PACKAGENAME___Plugin: ZPAnalyticsProviderProtocol {
+    
+    
+}
